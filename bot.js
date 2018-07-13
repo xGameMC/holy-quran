5ffcc8dcd195a15ac956cef0400e0fc11c9cf4a3
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 07:08:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`RENAD`,"http://twitch.tv/S-F")
+client.user.setGame(``,"")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
