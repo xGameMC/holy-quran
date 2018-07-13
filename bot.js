14333020e778b9d92f75d93d68efaff34b1682bd
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 06:55:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,13 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message', message =>  {
+                       var prefix = "&"
+         if(message.content.startsWith(prefix + 'n')) {
+        if (!message.member.voiceChannel) return message.channel.send('**I\'m sorry but you need to be in a voice channel**');
+        message.member.voiceChannel.join().then(message.channel.send(':ok:'));
+    
+    } 
+    });
 
 client.login(process.env.BOT_TOKEN);
