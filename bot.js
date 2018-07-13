7012d9b8d50f9d78f739b94cc9d55cf0059ebd36
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 07:12:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(``,"")
+client.user.setGame(`quran alkream`,"")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
